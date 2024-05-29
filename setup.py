84d2537967d61b805f46505ba3b31bf35664c1e9
Braian <braian7410@gmail.com>
2024-05-29 12:57:43 -0500

    Project packed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='validatorGP',
       version='0.1',
       description='A python package for validate user before use conection to database',
-      url='',
+      url='https://github.com/braianmobiledev/SoftwareDevelopmentGoodPractices',
       author='Braian Peña',
       author_email='braian7410@gmail.com',
       license='Apache License',
